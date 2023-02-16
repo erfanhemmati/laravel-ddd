9680d511588d01d1ca396d22a46dda1b06f07668
Erfan Hemmati <erfan.g2tr@gmail.com>
2023-02-16 13:59:18 +0330

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \Domain\User\Models\User::factory(10)->create();
+
+        // \Domain\User\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
